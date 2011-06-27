11c39bb33de050f1d9204898cb82d9384a5a99e9
Thomas Schreiber <ubiquill@gmail.com>
2011-06-26 22:19:07 -0700

    query is now seamlessly intigrated

--- lib/query.py
+++ lib/query.py
@@ -6,11 +6,35 @@
 # A collection of functions to help search the AUR
 
 import sys
+import os
 import json
 
 from twisted.internet import reactor
 from twisted.web.client import getPage
 from twisted.python.util import println
+
+
+class queryAUR:
+    """Searches the AUR using the scripting API"""
+
+    def __init__(self):
+        self.AURURL = 'http://aur.archlinux.org/rpc.php?type=search&arg='
+
+    def search(self, term):
+        queryURL = self.AURURL + term
+        
+        getPage(queryURL).addCallbacks(
+            callback=lambda value:(self.decodeResponse(value),reactor.stop()),
+            errback=lambda error:(println("an error occured",error),reactor.stop()))
+
+        reactor.run()
+
+    def decodeResponse(self, value):
+        jsonValue = json.loads(value);
+        for app in jsonValue['results']:
+            print 'aur/' + app['Name'] + ' ' + app['Version']
+            print '    ' + app['Description']
+        #print jsonValue['results']
 
 if (__name__ == "__main__"):
   
@@ -19,12 +43,8 @@ if (__name__ == "__main__"):
         sys.exit()
     else:
         argument = sys.argv[1]
+ 
+    os.system('/usr/bin/pacman -Ss ' + argument)
 
-    queryUrl = 'http://aur.archlinux.org/rpc.php?type=search&arg=' 
-    queryUrl += argument
-
-    getPage(queryUrl).addCallbacks(
-        callback=lambda value:(println(value),reactor.stop()),
-        errback=lambda error:(println("an error occured",error),reactor.stop()))
-
-    reactor.run()
+    query = queryAUR()
+    query.search(argument)
