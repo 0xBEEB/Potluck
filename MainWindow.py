7984ab2c7f4f50309771e07794ce0a3bc6aebb1c
Thomas Schreiber <ubiquill@gmail.com>
2011-07-01 18:38:57 -0700

    created mainwindow file

---
+++ MainWindow.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2
+# coding=UTF-8
+
+# Copyright © 2011 Thomas Schreiber
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# MainWindow.py
+# by Thomas Schreiber <ubiquill@gmail.com>
+#
+# The main window for potluck, an AUR GUI frontend.
+
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
+
+import sys
+
+class Main(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mw = Main()
+    mw.show()
+    sys.exit(app.exec_())
