ab89a9a86fe330383b8ac05d10cac19cb37d2d0c
Thomas Schreiber <ubiquill@gmail.com>
2011-07-05 12:03:39 -0700

    Changed mainwindow layout to accept filling parent on window resize

--- protoUi.py
+++ protoUi.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'proto.ui'
 #
-# Created: Tue Jul  5 11:45:51 2011
+# Created: Tue Jul  5 12:02:57 2011
 #      by: PyQt4 UI code generator 4.8.4
 #
 # WARNING! All changes made in this file will be lost!
@@ -18,7 +18,7 @@ class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
         MainWindow.setWindowModality(QtCore.Qt.NonModal)
-        MainWindow.resize(439, 414)
+        MainWindow.resize(484, 434)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -31,25 +31,25 @@ class Ui_MainWindow(object):
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.centralwidget.sizePolicy().hasHeightForWidth())
         self.centralwidget.setSizePolicy(sizePolicy)
+        self.centralwidget.setMaximumSize(QtCore.QSize(16777204, 16777215))
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.verticalLayoutWidget = QtGui.QWidget(self.centralwidget)
-        self.verticalLayoutWidget.setGeometry(QtCore.QRect(10, 10, 421, 381))
-        self.verticalLayoutWidget.setObjectName(_fromUtf8("verticalLayoutWidget"))
-        self.verticalLayout = QtGui.QVBoxLayout(self.verticalLayoutWidget)
-        self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetMaximumSize)
-        self.verticalLayout.setMargin(0)
+        self.gridLayout = QtGui.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetNoConstraint)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
         self.horizontalLayout = QtGui.QHBoxLayout()
         self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
-        self.queryEdit = QtGui.QLineEdit(self.verticalLayoutWidget)
+        self.queryEdit = QtGui.QLineEdit(self.centralwidget)
         self.queryEdit.setInputMask(_fromUtf8(""))
         self.queryEdit.setObjectName(_fromUtf8("queryEdit"))
         self.horizontalLayout.addWidget(self.queryEdit)
-        self.queryButton = QtGui.QPushButton(self.verticalLayoutWidget)
+        self.queryButton = QtGui.QPushButton(self.centralwidget)
+        self.queryButton.setEnabled(True)
         self.queryButton.setObjectName(_fromUtf8("queryButton"))
         self.horizontalLayout.addWidget(self.queryButton)
         self.verticalLayout.addLayout(self.horizontalLayout)
-        self.queryList = QtGui.QTreeWidget(self.verticalLayoutWidget)
+        self.queryList = QtGui.QTreeWidget(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -65,7 +65,11 @@ class Ui_MainWindow(object):
         self.queryList.header().setSortIndicatorShown(True)
         self.queryList.header().setStretchLastSection(True)
         self.verticalLayout.addWidget(self.queryList)
+        self.gridLayout.addLayout(self.verticalLayout, 0, 0, 1, 1)
         MainWindow.setCentralWidget(self.centralwidget)
+        self.statusBar = QtGui.QStatusBar(MainWindow)
+        self.statusBar.setObjectName(_fromUtf8("statusBar"))
+        MainWindow.setStatusBar(self.statusBar)
 
         self.retranslateUi(MainWindow)
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
