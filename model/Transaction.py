5d88f73c5a2fbc7665eba4683012bf36bf56cba1
Thomas Schreiber <ubiquill@gmail.com>
2011-07-12 17:54:31 -0700

    Somehow forgot to add this file

---
+++ model/Transaction.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python2
+# coding=UTF-8
+
+# Copyright © 2011 Thomas Schreiber
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+# Potluck
+# by Thomas Schreiber <ubiquill@gmail.com>
+
+import Aur
+import Pacman
+from PyQt4.QtGui import * 
+
+class Transaction:
+    def __init__(self):
+        self.queryResult = []
+        
+
+    def sync(self):
+        Pacman.sync()
+
+
+    #def upgrade(self):
+
+
+    def query(self, term):
+        result = []
+        installedList = Pacman.getInstalled()
+
+        pDict = Pacman.search(term)
+        for app in pDict:
+            item = app
+            if item['Name'] in installedList:
+                item['Installed'] = True
+            else:
+                item['Installed'] = False
+            result.append(app)
+
+        aurQuery = Aur.Query(term)
+        aurList = aurQuery.query
+        for app in aurList:
+            item = app
+            item['repo'] = 'aur'
+            if item['Name'] in installedList:
+                item['Installed'] = True
+            else:
+                item['Installed'] = False
+            result.append(app)
+
+        self.queryResult = result
+        
+
+
+    def remove(self, app):
+        Pacman.remove(app)
